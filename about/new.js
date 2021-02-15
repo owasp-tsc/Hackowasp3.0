ec824937308406ef75fe4ab469981f66b896a463
Rishabh <rjain_be19@thapar.edu>
2021-02-15 11:54:57 +0530

    final

--- about/new.js
+++ about/new.js
@@ -1,4 +1,4 @@
 function ShowContent1() {
-    document.querySelctor('.para2').style.display="none";
-    document.querySelector('.parax').style.display="block";
-} 
+  document.querySelector(".para2").style.display = "none";
+  document.querySelector(".parax").style.display = "block";
+}
---
+++ final/assets/js/aboutus.js
@@ -0,0 +1,4 @@
+function ShowContent1() {
+  document.querySelector(".para2").style.display = "none";
+  document.querySelector(".parax").style.display = "block";
+}
